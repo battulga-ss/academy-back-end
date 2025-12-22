2495571b3ec98415a7d87d6a77aff47fcc80d0fe
back-02-04 <back-02-04@Erxess-Mac-mini.local>
2025-12-22 20:14:49 +0800

    movie

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-const text: string = "Hello TypeScript!";
+import { MongoClient } from "mongodb";
 
-console.log(text);
+const uri =
+  "mongodb+srv://blessyou100x_db_user:8M13Savg2vLl3UUr@backend.b2ndaz2.mongodb.net/?appName=backend";
+
+const client = new MongoClient(uri);
+
+const run = async () => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const aa = await client.db("sample_mflix").collection("movies").findOne();
+    console.log(aa);
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+};
+
+run();
