a084ededc389aead51527bc74257b862c680c7a2
back-02-04 <back-02-04@Erxess-Mac-mini.local>
2026-01-05 20:55:07 +0800

    apollo

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,11 @@
-import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import { movieRouter } from "./movies/routes.ts";
+import { typeDefs, resolvers } from "./apolloServer.ts";
 
-// Express app
-const app = express();
-app.use(bodyParser.json());
-
-app.use("/movie", movieRouter);
-// app.use("./addMovies", addMovieRouter);
-// MongoDB connection
-await mongoose
+mongoose
   .connect(
-    "mongodb+srv://blessyou100x_db_user:8M13Savg2vLl3UUr@backend.b2ndaz2.mongodb.net/sample_mflix?appName=backend"
+    "mongodb+srv://blessyou100x_db_user:8M13Savg2vLl3UUr@backend-lesson.pfxqeun.mongodb.net/sample_mflix"
   )
   .then(() => {
     console.log("MongoDB connected");
@@ -21,4 +14,26 @@ await mongoose
     console.error("MongoDB connection error:", err);
   });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+export interface IContext {
+  user: {
+    firstname: string;
+  };
+}
+
+const server = new ApolloServer<IContext>({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    return {
+      user: {
+        firstname: "bat",
+      },
+    };
+  },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
